2f05c659beb1c9b6ecb81ef000641a3611d948f3
Estefania Montoya Davila <142418723+Estefmd@users.noreply.github.com>
2024-10-16 21:08:05 -0500

    Atributos
    Se crean Atributos en clases Cliente y administrador

--- src/main/java/co/edu/uniquindio/BookYourStay/model/Administrador.java
+++ src/main/java/co/edu/uniquindio/BookYourStay/model/Administrador.java
@@ -1,4 +1,20 @@
 package co.edu.uniquindio.BookYourStay.model;
 
+import lombok.Builder;
+import lombok.Getter;
+import lombok.Setter;
+import lombok.AllArgsConstructor;
+
+@Getter
+@Setter
+@Builder
+@AllArgsConstructor
+
 public class Administrador extends Usuario{
+
+    private String nombre;
+    private String cedula;
+    private String correo;
+    private String contrasenia;
+
 }
--- src/main/java/co/edu/uniquindio/BookYourStay/model/Cliente.java
+++ src/main/java/co/edu/uniquindio/BookYourStay/model/Cliente.java
@@ -1,4 +1,21 @@
 package co.edu.uniquindio.BookYourStay.model;
 
+import lombok.Builder;
+import lombok.Getter;
+import lombok.Setter;
+import lombok.AllArgsConstructor;
+
+@Getter
+@Setter
+@Builder
+@AllArgsConstructor
+
 public class Cliente extends Usuario {
+
+    private String nombre;
+    private String cedula;
+    private String telefono;
+    private String correo;
+    private String contrasenia;
+    private Billetera billetera;
 }
